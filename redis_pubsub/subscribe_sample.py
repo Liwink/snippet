51763121b2326448ed7b2644c7b98a645bfbd80c
Liwink <lyh.next@gmail.com>
2015-11-08 21:51:06 +0800

    gp

--- redis_pubsub/subscribe_sample.py
+++ redis_pubsub/subscribe_sample.py
@@ -5,12 +5,11 @@ __author__ = 'Liwink'
 
 import sys
 import os
-import time
+import datetime
 from redis import StrictRedis as Redis
 
 
 def _subscribe(channel):
-    r = Redis(host="localhost", port=6379, db=0)
 
     while True:
         try:
@@ -22,20 +21,24 @@ def _subscribe(channel):
 
 
 def _fork_and_subscribe(channel):
+
+    r.sadd(channel, 'first_subscriber')
+
     child_pid = os.fork()
     if not child_pid:
         try:
-            print("Processing work since {}".format(time.time()))
-            _subscribe(channel)
+            print("Processing work since {}".format(datetime.datetime.utcnow()))
+            _subscribe("{}:first_subscriber".format(channel))
         except Exception, e:
             print(e)
             sys.exit(1)
         sys.exit(0)
     else:
-        print("Forked {} at {}".format(child_pid, time.time()))
+        print("Forked {} at {}".format(child_pid, datetime.datetime.utcnow()))
         # os.waitpid(child_pid, 0)
 
 if __name__ == "__main__":
+    r = Redis(host="localhost", port=6379, db=0)
     print(sys.argv[1:])
     for channel in sys.argv[1:]:
         print(channel)
