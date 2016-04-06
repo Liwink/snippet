b89a7412e36e6fabdc398fb0d6a6fde8c2a4edf8
Liwink <lyh.next@gmail.com>
2016-04-06 10:35:52 +0800

    tornado queues get() return Future

---
+++ message_queue/tornado_queues.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Liwink'
+
+"""
+>>> import queue
+>>> q = queue.Queue()
+>>> q.get()
+... # wait (and block?
+"""
+
+"""
+>>> from tornado import queues
+>>> q = queues.Queue()
+>>> q.get()
+<tornado.concurrent.Future at 0x10ef62d30>
+"""
+
