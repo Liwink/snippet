66dcc958510b8836f8c3664734e001d025971148
Liwink <lyh.next@gmail.com>
2016-05-11 23:38:34 +0800

    Update redis_cli.py: class Redis

--- socket/redis_cli.py
+++ socket/redis_cli.py
@@ -3,10 +3,19 @@
 
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCKET_STREAM, 0)
-sock.setsockopt(socket.TCP, socket.TCP_NODELAY, 1)
+class Redis():
+    def __init__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
 
-sock.connect(("localhost", 6379))
+        sock.connect(("localhost", 6379))
+        self.sock = sock
 
-sock.sendall(b'*2\r\n$3\r\nGET\r\n$5\r\nbaidu\r\n')
-sock.recv(1024)
+    def get(self, key):
+        command = "*2\r\n$3\r\nGET\r\n$5\r\n{0}\r\n".format(key).encode()
+        self.sock.sendall(command)
+        response = self.sock.recv(1024).decode()
+        i = int(response[1])
+        return response[4:4+i]
+
+
