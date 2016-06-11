2cfebd837c53216402ef4758bfee2edac4f1a983
Liwink <lyh.next@gmail.com>
2016-06-11 22:44:07 +0800

    Add shared_scope

---
+++ metaprogramming/shared_scope.rb
@@ -0,0 +1,14 @@
+def def_methods
+  shared = 0
+  Kernel.send :define_method, :counter do
+    shared
+  end
+  Kernel.send :define_method, :inc do |x|
+    shared += x
+  end
+end
+def_methods
+
+p counter
+p inc(4)
+p counter
