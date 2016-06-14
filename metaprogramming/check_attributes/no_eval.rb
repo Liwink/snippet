b31cfe833029a136d33f59fc4eb1c1ddeede3d20
Liwink <lyh.next@gmail.com>
2016-06-14 09:00:05 +0800

    check_attributes: eval, method, block

---
+++ metaprogramming/check_attributes/no_eval.rb
@@ -0,0 +1,34 @@
+require 'test/unit'
+
+class Person; end
+
+class TestCheckAttribute < Test::Unit::TestCase
+  def setup
+    add_checked_attribute(Person, :age)
+    @bob = Person.new
+  end
+
+  def test_accepts_valid_values
+    @bob.age = 20
+    assert_equal 20, @bob.age
+  end
+
+  def test_refuses_nil_values
+    assert_raise RuntimeError, 'Invalid attribute' do
+      @bob.age = nil
+    end
+  end
+
+  def add_checked_attribute(klass, attribute)
+    klass.class_eval do
+      define_method attribute do
+        instance_variable_get("@#{attribute}")
+      end
+
+      define_method "#{attribute}=" do |value|
+        raise "Invalid attribute" unless value
+        instance_variable_set("@#{attribute}", value)
+      end
+    end
+  end
+end
