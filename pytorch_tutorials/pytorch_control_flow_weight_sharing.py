c42d34817313d0d5bb82a5322af31c9a405af399
Liwink <lyh.next@gmail.com>
2017-09-21 10:47:39 +0800

    done pytorch-examples

---
+++ pytorch_tutorials/pytorch_control_flow_weight_sharing.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import random
+import torch
+from torch.autograd import Variable
+
+
+class DynamicNet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        super(DynamicNet, self).__init__()
+        self.input_linear = torch.nn.Linear(D_in, H)
+        self.middle_linear = torch.nn.Linear(H, H)
+        self.output_linear = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        """
+        For the forward pass of the model, we randomly choose either 0, 1, 2, or 3
+        and reuse the middle_linear Module that many times to compute hidden layer
+        representations.
+
+        Since each forward pass builds a dynamic computation graph, we can use normal
+        Python control-flow operations like loops or conditional statements when
+        defining the forward pass of the model.
+
+        Here we also see that it is perfectly safe to reuse the same Module many
+        times when defining a computational graph. This is a big improvement from Lua
+        Torch, where each Module could be used only once.
+        """
+        h_relu = self.input_linear(x).clamp(min=0)
+        for _ in range(random.randint(0, 3)):
+            h_relu = self.middle_linear(h_relu).clamp(min=0)
+        y_pred = self.output_linear(h_relu)
+        return y_pred
+
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+x = Variable(torch.randn(N, D_in))
+y = Variable(torch.randn(N, D_out), requires_grad=False)
+
+model = DynamicNet(D_in, H, D_out)
+
+criterion = torch.nn.MSELoss(size_average=False)
+learning_rate = 1e-4
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+for t in range(500):
+    y_pred = model(x)
+
+    loss = criterion(y_pred, y)
+    print(t, loss.data[0])
+
+    optimizer.zero_grad()
+
+    loss.backward()
+    optimizer.step()
